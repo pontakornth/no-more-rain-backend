50983fe8748f5d39ed016e61ad8ba9dc99bbc989
pontakornth <pontakorn61@gmail.com>
2022-11-21 16:30:29 +0700

    Add types to controller.py

--- controller.py
+++ controller.py
@@ -2,8 +2,27 @@ import sys
 sys.path.append('stub')
 from swagger_server.models import *
 
-def search():
+def search(keywords, geolocation, search_radius=None, number_of_result=None, page=None):
+    """
+
+    :param keywords:
+    :type keywords: str
+    :param geolocation:
+    :type geolocation: str
+    :param search_radius:
+    :type search_radius: float
+    :param number_of_result:
+    :type number_of_result: float
+    :param page:
+    :type page: float
+
+    :rtype: List[AttractionSearchResult]
+    """
     return
 
-def get_attraction_detail():
+def get_attraction_detail(attraction_id: str):
+    """
+
+    :rtype: AttractionDetailResult
+    """
     return
