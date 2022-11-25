fb2119beba9daed386db8a899eb62788318c7b21
pontakornth <pontakorn61@gmail.com>
2022-11-25 10:57:06 +0700

    Add CORS

--- app.py
+++ app.py
@@ -3,9 +3,15 @@ import connexion
 
 sys.path.append('stub')
 from stub.swagger_server import encoder
+from flask_cors import CORS
 
 def main():
     app = connexion.App(__name__, specification_dir='./openapi')
+
+    # All domain, all routes.
+    # Please don't use this on production.
+    # This is a temporary solution so it's fine.
+    CORS(app.app)
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('no-more-rain.yaml',
                 arguments={'title': 'No more rain API'},
